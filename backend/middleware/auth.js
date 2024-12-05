1ea810956b7e5d39ffea3173df7802004c7374cc
Alankrit K <alankritkhatri9@gmail.com>
2024-12-05 16:04:14 +0530

    minor changes

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -5,7 +5,7 @@ export const authenticateToken = async (req, res, next) => {
   try {
     const authHeader = req.headers["authorization"];
     const token = authHeader && authHeader.split(" ")[1];
-
+    // console.log(token);
     if (!token) {
       return res.status(401).json({ message: "Authentication required" });
     }
